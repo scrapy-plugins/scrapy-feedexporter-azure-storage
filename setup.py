faccb6b37c34a241d7b234173bf60ed5c9256d6d
Imran Khan <imran.khan@zyte.com>
2022-07-13 15:31:34 +0500

    - Created setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="scrapy_azure_exporter",
+    version="0.0.2",
+    description="Scrapy Feed Exporter for Azure Storage",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[
+        "azure-storage-blob",
+        "scrapy"
+    ],
+)
